d813f2c40fcdd438371be0932a52bef67cd904bd
jysong <aixenhuh@gmail.com>
2022-03-20 22:27:28 +0900

    commit

---
+++ src/com/solve/backjoon/A1000.java
@@ -0,0 +1,10 @@
+package com.solve.backjoon;
+
+import java.util.Scanner;
+
+public class A1000 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextInt() + in.nextInt());
+    }
+}
